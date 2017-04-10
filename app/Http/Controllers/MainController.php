2d808e96a7397290b1a099a1e430d83d4acd751e
Plumes <zhaohonghao2010@gmail.com>
2017-04-10 10:16:20 +0000

    sub unsub

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -29,7 +29,7 @@ class MainController extends Controller
     public function crawl() {
         $member_id = "01";
         $fans_chat_list = DB::table('idol_fans_relation')->where('member_id', intval($member_id))->get();
-        $reply = 'test 发表了新的日记 <b>1234</b><a href=\"http://www.baidu.com\">查看详情</a>';
+        $reply = 'test 发表了新的日记 <b>1234</b><a href="http://www.baidu.com">查看详情</a>';
         $i=0;
         foreach ($fans_chat_list as $chat) {
             dispatch( (new sendUpdateMessageJob($chat->chat_id, $reply))->delay($i++/10) );
