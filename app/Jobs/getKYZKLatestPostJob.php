59f52b53208ddab3123c34d105b3aab112f4b164
Plumes <zhaohonghao2010@gmail.com>
2017-05-06 02:28:29 +0000

    regex deal

--- app/Jobs/getKYZKLatestPostJob.php
+++ app/Jobs/getKYZKLatestPostJob.php
@@ -36,37 +36,37 @@ class getKYZKLatestPostJob extends Job
         if(empty($member)) return;
         $blog_url = "http://www.keyakizaka46.com/s/k46o/diary/member/list?ima=0000&ct=".sprintf("%02d", $member->official_id);
         $html = file_get_contents($blog_url);
+        preg_match('/<article>(.*)<\/article>/uUs', $html, $matches);
         $dom = new \DOMDocument();
         libxml_use_internal_errors(true);
-        $dom->loadHTML($html);
+        $dom->loadHTML($matches[0]);
         $xpath = new \DOMXPath($dom);
 
-        $article = $xpath->query("//article")[0];
-        $title_node = $xpath->query("div[@class='innerHead']/div[@class='box-ttl']", $article)->item(0);
+        $title_node = $xpath->query("//div[@class='innerHead']/div[@class='box-ttl']")->item(0);
         $title = trim($xpath->query('h3/a', $title_node)->item(0)->nodeValue);
         $post_url = $xpath->query('h3/a/@href', $title_node)->item(0)->nodeValue;
         $post_url = "http://www.keyakizaka46.com".$post_url;
         $member_name = trim($xpath->query('p', $title_node)->item(0)->nodeValue);
-        $content = $xpath->query("div[@class='box-article']", $article)->item(0);
+        $content = $xpath->query("//div[@class='box-article']")->item(0);
         $content_html = $dom->saveXML($content);
-        $content_html = preg_replace('/<div .*>/', '', $content_html);
-        $content_html = preg_replace('/<\/div>/', '\n', $content_html);
-        $content_html = preg_replace('/<br\/>/', '\n', $content_html);
+//        $content_html = preg_replace('/<div .*>/', '', $content_html);
+//        $content_html = preg_replace('/<\/div>/', '\n', $content_html);
+//        $content_html = preg_replace('/<br\/>/', '\n', $content_html);
         preg_match('/<img.+src="(\S+)"/U', $content_html, $matches);
         $cover_image = false;
         if(isset($matches[1])) {
             $cover_image = $matches[1];
         }
-        $post_time = trim($xpath->query("div[@class='box-bottom']/ul/li", $article)->item(0)->textContent);
+        $post_time = trim($xpath->query("//div[@class='box-bottom']/ul/li")->item(0)->textContent);
         $now = date('Y-m-d H:i:s');
         $post = DB::table('posts')->where('url_hash', md5($post_url))->first();
         if(!empty($post)) return;
         DB::table('posts')->insert([
                 'member_id' => $member->id,
-                'title' => $title,
+                'title' => utf8_decode($title),
                 'url' => $post_url,
                 'url_hash' => md5($post_url),
-                'content' => trim($content_html),
+                'content' => utf8_decode(trim($content_html)),
                 'cover_image' => $cover_image!==false?$cover_image:'',
                 'posted_at' => $post_time,
                 'created_at'=>$now,
