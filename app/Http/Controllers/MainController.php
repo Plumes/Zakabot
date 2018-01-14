1513a80d73fca98539d7f262169aa082e214954c
Plumes <plumes@outlook.com>
2018-01-14 19:00:52 +0800

    upload image to weibo

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -168,8 +168,10 @@ class MainController extends Controller
             $post->content = preg_replace($search_pattern,$replace_pattern,$post->content);
         }
 
-        $replace_pattern = '<a$1><div class="fixed-height-container"><amp-img class="contain" layout="fill" src="$2"></amp-img></div></a>';
+        $replace_pattern = '<div class="fixed-height-container"><a$1><amp-img class="contain" layout="fill" src="$2"></amp-img></a></div>';
         $post->content = preg_replace("/<a(.*)><img.+src=\"([\w,:,\/,\.]+)\".*><\/a>/U", $replace_pattern, $post->content);
+        $replace_pattern = '<div class="fixed-height-container"><amp-img class="contain" layout="fill" src="$1"></amp-img></div>';
+        $post->content = preg_replace("/<img.+src=\"([\w,:,\/,\.]+)\".*>/U", $replace_pattern, $post->content);
         if(mb_strlen($post->title)>20) {
             $post->abbr_title = (mb_substr($post->title,0,20))."...";
         } else {
